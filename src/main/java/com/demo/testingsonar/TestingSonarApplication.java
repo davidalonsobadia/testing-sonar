470660e2508e9fb30a6391bc4d0db33b114d18fa
David Alonso <david.alonsobadia@gmail.com>
2018-06-04 00:35:42 +0200

    Correct Sonar code smell

--- src/main/java/com/demo/testingsonar/TestingSonarApplication.java
+++ src/main/java/com/demo/testingsonar/TestingSonarApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestingSonarApplication {
 
     public static void main(String[] args) {
-        String s = "this is a test";
         SpringApplication.run(TestingSonarApplication.class, args);
     }
 }
