8b7c2591743e1c64dd26edca7af9469566888e1c
David Alonso <david.alonsobadia@gmail.com>
2018-06-04 00:05:10 +0200

    Test Sonar comments

--- src/main/java/com/demo/testingsonar/TestingSonarApplication.java
+++ src/main/java/com/demo/testingsonar/TestingSonarApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestingSonarApplication {
 
     public static void main(String[] args) {
+        String s = "this is a test";
         SpringApplication.run(TestingSonarApplication.class, args);
     }
 }
