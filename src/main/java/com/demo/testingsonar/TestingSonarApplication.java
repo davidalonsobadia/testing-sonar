f2a4464610bb6070bcda21ad42e697faa42dd154
David Alonso <david.alonsobadia@gmail.com>
2018-06-03 15:53:18 +0200

    Initial commit

---
+++ src/main/java/com/demo/testingsonar/TestingSonarApplication.java
@@ -0,0 +1,12 @@
+package com.demo.testingsonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingSonarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingSonarApplication.class, args);
+    }
+}
---
+++ src/test/java/com/demo/testingsonar/TestingSonarApplicationTests.java
@@ -0,0 +1,16 @@
+package com.demo.testingsonar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestingSonarApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
